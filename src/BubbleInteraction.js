7691dca365dd1842482c4cd618d2f4be6f211b82
Dario Breitenstein <online@imakethings.ch>
2021-07-20 14:38:12 +0200

    refactor to use js class syntax

--- src/BubbleInteraction.js
+++ src/BubbleInteraction.js
@@ -1,79 +1,66 @@
-;(function () {
+const canvasWrapper = {
+    screen: {
+        elem: null,
+        callback: null,
+        ctx: null,
+        width: 0,
+        height: 0,
+        left: 0,
+        top: 0,
+        init (id, callback, initRes) {
+            this.elem = document.getElementById(id);
+            this.callback = callback || null;
+            if (this.elem.tagName === "CANVAS") this.ctx = this.elem.getContext("2d");
 
-    "use strict";
-
-    let lava0;
-    let lava1;
-    let lava2;
-    let lava3;
-
-    let canvasTag;
-
-    var ge1doot = {
-        screen: {
-            elem: null,
-            callback: null,
-            ctx: null,
-            width: 0,
-            height: 0,
-            left: 0,
-            top: 0,
-            init: function (id, callback, initRes) {
-                this.elem = document.getElementById(id);
-                this.callback = callback || null;
-                if (this.elem.tagName === "CANVAS") this.ctx = this.elem.getContext("2d");
-
-                window.addEventListener('resize', function () {
-                    this.resize();
-                }.bind(this), false);
+            window.addEventListener('resize', function () {
+                this.resize();
+            }.bind(this), false);
 
 
-                this.elem.onselectstart = function () {
-                    return false;
-                }
+            this.elem.onselectstart = () => false;
+            this.elem.ondrag = () => false;
 
-                this.elem.ondrag = function () {
-                    return false;
-                }
-
-                initRes && this.resize();
-                return this;
-            },
-            resize: function () {
-                var o = this.elem;
-                this.width = o.offsetWidth;
-                this.height = o.offsetHeight;
-                for (this.left = 0, this.top = 0; o != null; o = o.offsetParent) {
-                    this.left += o.offsetLeft;
-                    this.top += o.offsetTop;
-                }
-                if (this.ctx) {
-                    this.elem.width = this.width;
-                    this.elem.height = this.height;
-                }
-                this.callback && this.callback();
+            initRes && this.resize();
+            return this;
+        },
+        resize() {
+            let o = this.elem;
+            this.width = o.offsetWidth;
+            this.height = o.offsetHeight;
+            for (this.left = 0, this.top = 0; o != null; o = o.offsetParent) {
+                this.left += o.offsetLeft;
+                this.top += o.offsetTop;
             }
+            if (this.ctx) {
+                this.elem.width = this.width;
+                this.elem.height = this.height;
+            }
+            this.callback && this.callback();
         }
     }
+};
 
-    // Point constructor
-    var Point = function (x, y) {
+class Point {
+    constructor(x, y) {
         this.x = x;
         this.y = y;
         this.magnitude = x * x + y * y;
         this.computed = 0;
         this.force = 0;
-    };
-    Point.prototype.add = function (p) {
-        return new Point(this.x + p.x, this.y + p.y);
-    };
+    }
 
-    // Ball constructor
-    var Ball = function (parent) {
-        var min = .1;
-        var max = 1.5;
+    add(p) {
+        return new Point(this.x + p.x, this.y + p.y);
+    }
+}
+
+class Ball {
+    constructor (parent) {
+        const min = .1;
+        const max = 1.5;
         this.vel = new Point(
-            (Math.random() > 0.5 ? 1 : -1) * (0.2 + Math.random() * 0.25), (Math.random() > 0.5 ? 1 : -1) * (0.2 + Math.random())
+            (Math.random() > 0.5 ? 1 : -1) * (0.2 + Math.random() * 0.25),
+            (Math.random() > 0.5 ? 1 : -1) * (0.2 + Math.random())
         );
         this.pos = new Point(
             parent.width * 0.2 + Math.random() * parent.width * 0.6,
@@ -82,23 +69,9 @@
         this.size = (parent.wh / 25) + (Math.random() * (max - min) + min) * (parent.wh / 25);
         this.width = parent.width;
         this.height = parent.height;
+    }
 
-        /*    canvasTag = document.getElementById("bubble").getContext("2d");
-            canvasTag.fillStyle = "#000";
-            canvasTag.translate(50,50);
-            canvasTag.arc(0,0,20,0,Math.PI*2);
-            canvasTag.fill();
-
-            // ctx.beginPath();
-            canvasTag.strokeStyle = "#F00";
-            canvasTag.arc(0,0,15,0,Math.PI*2);
-            canvasTag.stroke();*/
-
-    };
-
-    // move balls
-    Ball.prototype.move = function () {
-
+    move() {
         // bounce borders
         if (this.pos.x >= this.width - this.size) {
             if (this.vel.x > 0) this.vel.x = -this.vel.x;
@@ -118,11 +91,11 @@
 
         // velocity
         this.pos = this.pos.add(this.vel);
+    }
+}
 
-    };
-
-    // lavalamp constructor
-    const LavaLamp = function (width, height, color) {
+class LavaLamp {
+    constructor(width, height, color) {
         this.step = 5;
         this.width = width;
         this.height = height;
@@ -142,42 +115,37 @@
         this.sign = 1;
 
         // init grid
-        for (var i = 0; i < (this.sx + 2) * (this.sy + 2); i++) {
-            this.grid[i] = new Point(
-                (i % (this.sx + 2)) * this.step, (Math.floor(i / (this.sx + 2))) * this.step
-            )
-        }
+        this.grid = new Array((this.sx + 2) * (this.sy + 2)).fill(0)
+            .map((_,i) => new Point(
+            (i % (this.sx + 2)) * this.step,
+            (Math.floor(i / (this.sx + 2))) * this.step
+        ));
 
-   /*     // create bubbles
-        for (var k = 0; k < numBalls; k++) {
-            this.balls[k] = new Ball(this);
-        }*/
+        this.balls.push(new Ball(this));
+    }
 
-    };
-    // compute cell force
-    LavaLamp.prototype.computeForce = function (x, y, idx) {
-
-        var force;
-        var id = idx || x + y * (this.sx + 2);
+    // Compute cell force
+    computeForce(x, y, idx) {
+        let force;
+        const id = idx || x + y * (this.sx + 2);
 
         if (x === 0 || y === 0 || x === this.sx || y === this.sy) {
             force = 0.6 * this.sign;
         } else {
             force = 0;
-            var cell = this.grid[id];
-            var i = 0;
-            var ball;
+            const cell = this.grid[id];
+            let i = 0;
+            let ball;
             while (ball = this.balls[i++]) {
                 force += ball.size * ball.size / (-2 * cell.x * ball.pos.x - 2 * cell.y * ball.pos.y + ball.pos.magnitude + cell.magnitude);
             }
-            force *= this.sign
+            force *= this.sign;
         }
         this.grid[id].force = force;
         return force;
     };
 
-    // compute cell
-    LavaLamp.prototype.marchingSquares = function (next) {
+    marchingSquares (next) {
         var x = next[0];
         var y = next[1];
         var pdir = next[2];
@@ -232,7 +200,7 @@
         }
     };
 
-    LavaLamp.prototype.renderBubbles = function () {
+    renderBubbles() {
         var i = 0, ball;
         while (ball = this.balls[i++]) ball.move();
         // reset grid
@@ -264,86 +232,60 @@
             }
         }
     };
+}
 
-    /*    // gradients
-        var createRadialGradient = function (w, h, r, c0, c1) {
-            var gradient = ctx.createRadialGradient(
-                w / 1, h / 1, 0,
-                w / 1, h / 1, r
-            );
-            gradient.addColorStop(0, c0);
-            gradient.addColorStop(1, c1);
-            return gradient;
-        };*/
+const screen = canvasWrapper.screen.init("bubble", null, true);
+const ctx = screen.ctx;
+screen.resize();
 
-    // canvas
-    var screen = ge1doot.screen.init("bubble", null, true),
-        ctx = screen.ctx;
-    screen.resize();
-    // create LavaLamps
-    const clients = {};
+const clients = {};
 
-    // main loop
-    var run = function () {
-        requestAnimationFrame(run);
-        ctx.clearRect(0, 0, screen.width, screen.height);
+// main loop
+const run = () => {
+    requestAnimationFrame(run);
+    ctx.clearRect(0, 0, screen.width, screen.height);
 
+    // Render Bubbles for each LavaLamp
+    Object.values(clients).forEach(lavaLamp => lavaLamp.renderBubbles())
+};
 
-        Object.values(clients).forEach(client => client.renderBubbles())
-    };
+/*  lava0 = new LavaLamp(screen.width, screen.height, 3, "#FFD850");
+  lava1 = new LavaLamp(screen.width, screen.height, 3, "#FFA370");
+  lava2 = new LavaLamp(screen.width, screen.height, 3, "#FF7D7D");
+  lava3 = new LavaLamp(screen.width, screen.height, 3, "#05AFBA");*/
 
-
-
-  /*  lava0 = new LavaLamp(screen.width, screen.height, 3, "#FFD850");
-    lava1 = new LavaLamp(screen.width, screen.height, 3, "#FFA370");
-    lava2 = new LavaLamp(screen.width, screen.height, 3, "#FF7D7D");
-    lava3 = new LavaLamp(screen.width, screen.height, 3, "#05AFBA");*/
-
-    //////////////
 // Create a client instance
-    const client = new Paho.MQTT.Client("broker.hivemq.com", 8000, "bubbleClient34526246");
+const client = new Paho.MQTT.Client("broker.hivemq.com", 8000, `bubbleClient${new Date().getTime()}`);
 
 // set callback handlers
-    client.onConnectionLost = onConnectionLost;
-    client.onMessageArrived = onMessageArrived;
+client.onConnectionLost = (responseObject) => {
+    if (responseObject.errorCode !== 0) {
+        console.log("onConnectionLost:" + responseObject.errorMessage);
+    }
+};
+
+client.onMessageArrived = (message) => {
+    console.log(JSON.parse(message.payloadString));
+    const eventMessage = JSON.parse(message.payloadString);
+
+    switch (eventMessage.event) {
+        case 'esp32_client_connected':
+        case 'ble_client_connected':
+            if (eventMessage.device_address) {
+                clients[eventMessage.device_address] = new LavaLamp(screen.width, screen.height, "#FFD850");
+            }
+            break;
+        case 'notification_arrived':
+
+            break;
+    }
+};
 
 // connect the client
-    client.connect({onSuccess: onConnect});
+client.connect({onSuccess() {
+    // Once a connection has been made, make a subscription and send a message.
+    console.log("onConnect");
+    client.subscribe("8b4dac03-9840-46fb-8eaf-30bb4f4a8384");
+}});
 
-
-// called when the client connects
-    function onConnect() {
-        // Once a connection has been made, make a subscription and send a message.
-        console.log("onConnect");
-        client.subscribe("8b4dac03-9840-46fb-8eaf-30bb4f4a8384");
-    }
-
-// called when the client loses its connection
-    function onConnectionLost(responseObject) {
-        if (responseObject.errorCode !== 0) {
-            console.log("onConnectionLost:" + responseObject.errorMessage);
-        }
-    }
-
-// called when a message arrives
-    function onMessageArrived(message) {
-        console.log(JSON.parse(message.payloadString));
-        const eventMessage = JSON.parse(message.payloadString);
-
-        switch (eventMessage.event) {
-            case 'esp32_client_connected':
-            case 'ble_client_connected':
-                clients[eventMessage.device_address] = new LavaLamp(screen.width, screen.height, "#FFD850");
-
-                break;
-            case 'notification_arrived':
-
-                break;
-
-        }
-    }
-/////////////////////
-
-    run();
-
-})();
+run();
